6903d733501dc26c6ce64da037a0c4c6ecab9412
Francisco Fernandes <chicaofernandes@protonmail.com>
2018-07-09 11:36:27 -0300

    Added use case for list storage and created response and request objects

---
+++ pyromatic/use_cases/request_objects.py
@@ -0,0 +1,8 @@
+class StorageRoomListRequestObject(object):
+    @classmethod
+    def from_dict(cls, data):
+        return StorageRoomListRequestObject()
+
+    #????
+    def __nonzero__(self):
+        return True
