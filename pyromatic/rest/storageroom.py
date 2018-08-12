b4f0a5ac0689bcd1624cb1d83d878267ff26fcad
Francisco Fernandes <chicaofernandes@protonmail.com>
2018-08-12 10:21:59 -0300

    Handling of improper JSON data for creation

--- pyromatic/rest/storageroom.py
+++ pyromatic/rest/storageroom.py
@@ -18,21 +18,27 @@ STATUS_CODES = {
 }
 
 mem_repo = [
-    {'code': 'f853578c-fc0f-4e65-81b8-566c5dffa35a',
-     'size': 215,
-     'price': 39,
-     'longitude': '-0.09998975',
-     'latitude': '51.75436293'},
-    {'code': 'fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a',
-    'size': 405,
-    'price': 66,
-    'longitude': '0.18228006',
-    'latitude': '51.74640997'},
-    {'code': '913694c6-435a-4366-ba0d-da5334a611b2',
-    'size': 56,
-    'price': 60,
-    'longitude': '0.27891577',
-    'latitude': '51.45994069'}
+    {
+        'code': 'f853578c-fc0f-4e65-81b8-566c5dffa35a',
+        'size': 215,
+        'price': 39,
+        'longitude': '-0.09998975',
+        'latitude': '51.75436293'
+    },
+    {
+        'code': 'fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a',
+        'size': 405,
+        'price': 66,
+        'longitude': '0.18228006',
+        'latitude': '51.74640997'
+    },
+    {
+        'code': '913694c6-435a-4366-ba0d-da5334a611b2',
+        'size': 56,
+        'price': 60,
+        'longitude': '0.27891577',
+        'latitude': '51.45994069'
+    }
 ]
 
 repo = mr.MemRepo(mem_repo)
@@ -70,13 +76,23 @@ def storageroom(request):
 def create_storageroom(request):
     logger.info('[CALLED] storagerooms create')
 
-    storage_json = request.json_body
+    try:
+        storage_json = request.json_body
+    except json.decoder.JSONDecodeError as exc:
+        error = {'status': 400,
+                 'success': False,
+                 'message': 'Unexpected JSON format. Can\'t decode it'}
+
+        return Response(json=error,
+                        content_type='application/json',
+                        status=error['status'])
+
     request_object = req.StorageRoomCreateRequestObject.from_dict(storage_json)
     use_case = uc.StorageRoomCreateUseCase(repo)
     response = use_case.execute(request_object)
 
-    #logger.info('[RESPONSE:VALUE]: {}'.format(response.value))
-    #logger.info('[RESPONSE:TYPE]: {}'.format(response.type))
+    logger.info('[RESPONSE:VALUE]: {}'.format(response.value.__dict__))
+    logger.info('[RESPONSE:TYPE]: {}'.format(response.type))
 
     return Response(json=json.loads(json.dumps(response.value,
                                                cls=ser.StorageRoomEncoder)),
